6fcbd5648022af51d87865513d3bfbd4bda84935
Hetal Patel <nphetal1905@gmail.com>
2019-10-10 15:14:55 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
-
+import Navbar from "./components/layouts/Navbar";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Find coders</h1>
+        <Navbar />
+        {/* <h1>Find coders</h1> */}
       </div>
     );
   }
