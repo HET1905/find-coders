f7189ea21564ac74a6fe24073abdc2086786ec58
Hetal Patel <nphetal1905@gmail.com>
2019-10-11 16:21:01 -0400

    spinner added for loading

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,39 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 import UserItem from "./UserItem";
-class Users extends Component {
-  //   state = {
-  //     users: [
-  //       {
-  //         id: "1",
-  //         login: "Mojombo",
-  //         avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-  //         html_url: "https://github.com/mojombo"
-  //       },
-  //       {
-  //         id: "2",
-  //         login: "defunkt",
-  //         avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
-  //         html_url: "https://github.com/defunkt"
-  //       },
-  //       {
-  //         id: "3",
-  //         login: "pjhyett",
-  //         avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
-  //         html_url: "https://github.com/pjhyett"
-  //       }
-  //     ]
-  //   };
+import Spinner from "../layouts/Spinner";
 
-  render() {
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userSytyle}>
-        {this.props.users.map(user => (
+        {users.map(user => (
           <UserItem key={user.id} user={user} />
         ))}
+        {/* <Spinner /> */}
       </div>
     );
   }
-}
+};
+
+Users.protoTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
+};
 
 const userSytyle = {
   display: "grid",
