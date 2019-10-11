df6414fc967ef20b0104bd2e4158cc5b8e005d8d
Hetal Patel <nphetal1905@gmail.com>
2019-10-11 17:13:33 -0400

    search button and form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 import NavbarComp from "./components/layouts/NavbarComp";
 import Users from "./components/users/Users";
+import Search from "./components/users/Search";
 import "./App.css";
 
 class App extends Component {
@@ -11,7 +12,11 @@ class App extends Component {
   };
   async componentDidMount() {
     this.setState({ loading: true });
-    const res = await axios.get("https://api.github.com/users");
+    const res = await axios.get(
+      `https://api.github.com/users?
+      client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+      &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
 
     console.log(res.data);
     this.setState({ users: res.data, loading: false });
@@ -21,6 +26,7 @@ class App extends Component {
       <div className="container-fluid">
         <NavbarComp />
         <div className="container">
+          <Search />
           <Users users={this.state.users} loading={this.state.loading} />
         </div>
       </div>
---
+++ src/components/users/Search.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
+// import Button from "react-bootstrap/Button";
+
+class Search extends Component {
+  state = {
+    text: ""
+  };
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+  onSubmit = e => {
+    e.preventDefault();
+    console.log(this.state.text);
+  };
+  render() {
+    return (
+      <div>
+        <Form onSubmit={this.onSubmit}>
+          <Form.Control
+            type="text"
+            name="text"
+            value={this.state.text}
+            onChange={this.onChange}
+            placeholder="Search coders......."
+          />
+          {/* <Button variant="primary" type="submit">
+            Submit
+          </Button> */}
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-dark btn-block"
+          />
+        </Form>
+      </div>
+    );
+  }
+}
+
+export default Search;
